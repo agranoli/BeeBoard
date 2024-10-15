d6615aa5f595cca96e10043ca5177f77696ee277
Reinis Artimovics <124902374+agranoli@users.noreply.github.com>
2024-10-15 22:10:14 +0300

    converted images to .webp for maximum performance

--- Scripts/item-details.js
+++ Scripts/item-details.js
@@ -7,25 +7,25 @@ document.addEventListener('DOMContentLoaded', () => {
         1: {
             name: 'Elza',
             description: 'Kad jūties emocionāli pārņemts vai sasprindzis, Elza ar savām ledus spējām palīdz "atvēsināt" emocijas, ļaujot tev rast mieru un koncentrēties uz risinājumu. Viņa dod spēku saglabāt vēsu prātu un skaidri saskatīt, kā rīkoties pat sarežģītās situācijās.',
-            image: './Images/Photos/elsa.jpg',
+            image: './Images/Photos/elsa.webp',
             backgroundClass: 'recommended-bee-board-cont-elsa'
         },
         2: {
             name: 'Harijs Potters',
             description: 'Harijs Potters ir pazīstams ar savu drosmi un lojalitāti. Viņš iedvesmo mūs stāties pretī izaicinājumiem un aizstāvēt savus draugus.',
-            image: './Images/Photos/harry-potter.jpg',
+            image: './Images/Photos/harry-potter.webp',
             backgroundClass: 'recommended-bee-board-cont-potter'
         },
         3: {
             name: 'Stičs',
             description: 'Stičs ir jautrs un enerģisks, vienmēr gatavs piedzīvojumiem. Viņš atgādina mums par draudzības un ģimenes nozīmi.',
-            image: './Images/Photos/stitch.jpg',
+            image: './Images/Photos/stitch.webp',
             backgroundClass: 'recommended-bee-board-cont-stitch'
         },
         4: {
             name: 'Laime',
             description: 'Laime ir prieka un pozitīvisma iemiesojums. Viņa palīdz mums saskatīt labo katrā dienā un izbaudīt dzīves mazās lietas.',
-            image: './Images/Photos/joy.jpg',
+            image: './Images/Photos/joy.webp',
             backgroundClass: 'recommended-bee-board-cont-joy'
         }
     };
